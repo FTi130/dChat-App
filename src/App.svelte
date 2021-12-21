7b185e6561cc38199d0bf61ebbd91b1d91bcb9ed
Pavel <player49@mail.ru>
2021-12-21 01:41:35 +0100

    adding chat

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Header from './Header.svelte'
+	import Chat from './Chat.svelte'
+</script>
+
+<div class="app">
+<!--	обновлять приложение раз в минуту?-->
+	<Header />
+	<Chat />
+
+</div>
---
+++ src/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {username, user} from "./user";
+
+    function signout() {
+        user.leave();
+        user.set('');
+    }
+</script>
+
+<header>
+    <h1>ну, чат</h1>
+    {#if $username}
+        <div class="user-bio">
+            <span>
+                <strong>{$username}</strong>
+            </span>
+            <img src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt=" " />
+        </div>
+            <button class="signout-button" on:click={signout}>
+                Выйти
+            </button>
+
+        {:else}
+        <h3>Чат</h3>
+
+
+        {/if}
+</header>
